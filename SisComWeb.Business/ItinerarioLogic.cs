4123d7fc5c12351c50c80180730401f153991197
777sc <g.gomez.ingaruca@gmail.com>
2019-02-06 14:21:50 -0500

    MuestraPlano 001

--- SisComWeb.Business/ItinerarioLogic.cs
+++ SisComWeb.Business/ItinerarioLogic.cs
@@ -30,22 +30,22 @@ namespace SisComWeb.Business
                 }
 
                 // Recorre cada registro
-                foreach (var iti in resBuscarItinerarios.Valor)
+                for (int i = 0; i < resBuscarItinerarios.Valor.Count; i++)
                 {
                     // Calcula 'FechaProgramacion'
-                    var doubleDias = double.Parse(iti.Dias.ToString());
-                    if (iti.Dias > 0)
-                        iti.FechaProgramacion = DateTime.Parse(request.FechaViaje).AddDays(doubleDias).ToString();
+                    var doubleDias = double.Parse(resBuscarItinerarios.Valor[i].Dias.ToString());
+                    if (resBuscarItinerarios.Valor[i].Dias > 0)
+                        resBuscarItinerarios.Valor[i].FechaProgramacion = DateTime.Parse(request.FechaViaje).AddDays(doubleDias).ToString();
                     else
-                        iti.FechaProgramacion = DateTime.Parse(request.FechaViaje).ToString();
+                        resBuscarItinerarios.Valor[i].FechaProgramacion = DateTime.Parse(request.FechaViaje).ToString();
 
                     // Verifica cambios 'TurnoViaje'
-                    var resVerificaCambiosTurnoViaje = ItinerarioRepository.VerificaCambiosTurnoViaje(iti.NroViaje, iti.FechaProgramacion);
+                    var resVerificaCambiosTurnoViaje = ItinerarioRepository.VerificaCambiosTurnoViaje(resBuscarItinerarios.Valor[i].NroViaje, resBuscarItinerarios.Valor[i].FechaProgramacion);
                     if (resVerificaCambiosTurnoViaje.Estado && !string.IsNullOrEmpty(resVerificaCambiosTurnoViaje.Valor.NomServicio))
                     {
-                        iti.CodiServicio = resVerificaCambiosTurnoViaje.Valor.CodiServicio;
-                        iti.NomServicio = resVerificaCambiosTurnoViaje.Valor.NomServicio;
-                        iti.CodiEmpresa = resVerificaCambiosTurnoViaje.Valor.CodiEmpresa;
+                        resBuscarItinerarios.Valor[i].CodiServicio = resVerificaCambiosTurnoViaje.Valor.CodiServicio;
+                        resBuscarItinerarios.Valor[i].NomServicio = resVerificaCambiosTurnoViaje.Valor.NomServicio;
+                        resBuscarItinerarios.Valor[i].CodiEmpresa = resVerificaCambiosTurnoViaje.Valor.CodiEmpresa;
 
                         response.Mensaje += resVerificaCambiosTurnoViaje.Mensaje;
                     }
@@ -58,11 +58,10 @@ namespace SisComWeb.Business
                     }
 
                     // Busca 'ProgramacionViaje'
-                    var resBuscarProgramacionViaje = ItinerarioRepository.BuscarProgramacionViaje(iti.NroViaje, iti.FechaProgramacion);
+                    var resBuscarProgramacionViaje = ItinerarioRepository.BuscarProgramacionViaje(resBuscarItinerarios.Valor[i].NroViaje, resBuscarItinerarios.Valor[i].FechaProgramacion);
                     if (resBuscarProgramacionViaje.Estado && resBuscarProgramacionViaje.Valor != 0)
                     {
                         response.Mensaje += resBuscarProgramacionViaje.Mensaje;
-
                         // Obtiene 'BusProgramacion'
                         resObtenerBus = ItinerarioRepository.ObtenerBusProgramacion(resBuscarProgramacionViaje.Valor);
                         if (resObtenerBus.Estado)
@@ -77,10 +76,35 @@ namespace SisComWeb.Business
                     {
                         response.Mensaje += "Mensaje: resBuscarProgramacionViaje.Valor: es cero. ";
 
+
+                        // Valida 'SoloProgramados'
+                        if (request.SoloProgramados)
+                        {
+                            resBuscarItinerarios.Valor.RemoveAt(i);
+                            i--;
+                            continue;
+                        }
+
+
                         // Obtiene 'BusEstandar'
-                        resObtenerBus = ItinerarioRepository.ObtenerBusEstandar(iti.CodiEmpresa, iti.CodiSucursal, iti.CodiRuta, iti.CodiServicio, request.Hora);
+                        resObtenerBus = ItinerarioRepository.ObtenerBusEstandar(resBuscarItinerarios.Valor[i].CodiEmpresa, resBuscarItinerarios.Valor[i].CodiSucursal, resBuscarItinerarios.Valor[i].CodiRuta, resBuscarItinerarios.Valor[i].CodiServicio, request.Hora);
                         if (resObtenerBus.Estado)
-                            response.Mensaje += resObtenerBus.Mensaje;
+                        {
+                            // En caso de no encontrar resultado.
+                            if (!string.IsNullOrEmpty(resObtenerBus.Valor.CodiBus))
+                                response.Mensaje += resObtenerBus.Mensaje;
+                            else
+                            {
+                                resObtenerBus = ItinerarioRepository.ObtenerBusEstandar(resBuscarItinerarios.Valor[i].CodiEmpresa, resBuscarItinerarios.Valor[i].CodiSucursal, resBuscarItinerarios.Valor[i].CodiRuta, resBuscarItinerarios.Valor[i].CodiServicio, "");
+                                if (resObtenerBus.Estado)
+                                    response.Mensaje += resObtenerBus.Mensaje;
+                                else
+                                {
+                                    response.Mensaje += "Error: ObtenerBusEstandar. ";
+                                    return response;
+                                }
+                            }
+                        }
                         else
                         {
                             response.Mensaje += "Error: ObtenerBusEstandar. ";
@@ -94,19 +118,19 @@ namespace SisComWeb.Business
                     }
 
                     // Valida 'TurnoAdicional'
-                    var resValidarTurnoAdicional = ItinerarioRepository.ValidarTurnoAdicional(iti.NroViaje, iti.FechaProgramacion);
+                    var resValidarTurnoAdicional = ItinerarioRepository.ValidarTurnoAdicional(resBuscarItinerarios.Valor[i].NroViaje, resBuscarItinerarios.Valor[i].FechaProgramacion);
                     if (resValidarTurnoAdicional.Estado && resValidarTurnoAdicional.Valor == 1)
                     {
                         response.Mensaje += resValidarTurnoAdicional.Mensaje;
 
                         // Valida 'ViajeCalendario'
-                        var resValidarViajeCalendario = ItinerarioRepository.ValidarViajeCalendario(iti.NroViaje, iti.FechaProgramacion);
+                        var resValidarViajeCalendario = ItinerarioRepository.ValidarViajeCalendario(resBuscarItinerarios.Valor[i].NroViaje, resBuscarItinerarios.Valor[i].FechaProgramacion);
                         if (resValidarViajeCalendario.Estado)
                         {
                             response.Mensaje += resValidarViajeCalendario.Mensaje;
 
                             if (resValidarViajeCalendario.Valor == 1)
-                                break;
+                                continue;
                             else
                             {
                                 // Obtiene 'TotalVentas'
@@ -120,7 +144,7 @@ namespace SisComWeb.Business
                                 }
 
                                 // Lista 'TotalVentas'
-                                resListarPuntosEmbarque = ItinerarioRepository.ListarPuntosEmbarque(iti.CodiOrigen, iti.CodiDestino, iti.CodiServicio, iti.CodiEmpresa, iti.CodiPuntoVenta, request.Hora);
+                                resListarPuntosEmbarque = ItinerarioRepository.ListarPuntosEmbarque(resBuscarItinerarios.Valor[i].CodiOrigen, resBuscarItinerarios.Valor[i].CodiDestino, resBuscarItinerarios.Valor[i].CodiServicio, resBuscarItinerarios.Valor[i].CodiEmpresa, resBuscarItinerarios.Valor[i].CodiPuntoVenta, request.Hora);
                                 if (resListarPuntosEmbarque.Estado)
                                     response.Mensaje += resListarPuntosEmbarque.Mensaje;
                                 else
@@ -130,7 +154,7 @@ namespace SisComWeb.Business
                                 }
 
                                 // Lista 'PuntosArribo'
-                                resListarPuntosArribo = ItinerarioRepository.ListarPuntosArribo(iti.CodiOrigen, iti.CodiDestino, iti.CodiServicio, iti.CodiEmpresa, iti.CodiPuntoVenta, request.Hora);
+                                resListarPuntosArribo = ItinerarioRepository.ListarPuntosArribo(resBuscarItinerarios.Valor[i].CodiOrigen, resBuscarItinerarios.Valor[i].CodiDestino, resBuscarItinerarios.Valor[i].CodiServicio, resBuscarItinerarios.Valor[i].CodiEmpresa, resBuscarItinerarios.Valor[i].CodiPuntoVenta, request.Hora);
                                 if (resListarPuntosArribo.Estado)
                                     response.Mensaje += resListarPuntosArribo.Mensaje;
                                 else
@@ -150,7 +174,7 @@ namespace SisComWeb.Business
                     {
                         response.Mensaje += "Mensaje: resValidarTurnoAdicional: Valor es cero. ";
 
-                        break;
+                        continue;
                     }
                     else
                     {
