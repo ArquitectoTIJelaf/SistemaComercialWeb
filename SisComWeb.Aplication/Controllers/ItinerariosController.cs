fe9815cfd929823034ded11cd2fb55d942212980
Hector Salazar <hectorsalazarv@outlook.com>
2019-02-08 10:35:45 -0500

    changes in list of itinerarios

--- SisComWeb.Aplication/Controllers/ItinerariosController.cs
+++ SisComWeb.Aplication/Controllers/ItinerariosController.cs
@@ -2,6 +2,7 @@
 using SisComWeb.Aplication.Helpers;
 using SisComWeb.Aplication.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -51,12 +52,38 @@ namespace SisComWeb.Aplication.Controllers
                 if (estado)
                 {
                     JArray data = (JArray)tmpResult["Valor"];
-                    //List<SelectableEnumItem> items = ((JArray)tmpResult["Valor"]).Select(x => new SelectableEnumItem
-                    //{
-                    //    FirstName = (string)x["first_name"],
-                    //    Selected = (bool)x["selected"]
-                    //}).ToList();
-                    return Json(data, JsonRequestBehavior.AllowGet);
+                    List<Itinerario> items = ((JArray)tmpResult["Valor"]).Select(x => new Itinerario
+                    {
+                        AsientosVendidos = (int)x["AsientosVendidos"],
+                        CapacidadBus = (string)x["CapacidadBus"],
+                        CodiBus = (string)x["CodiBus"],
+                        CodiDestino = (int)x["CodiDestino"],
+                        CodiEmpresa = (int)x["CodiEmpresa"],
+                        CodiOrigen = (int)x["CodiOrigen"],
+                        CodiProgramacion = (int)x["CodiProgramacion"],
+                        CodiPuntoVenta = (int)x["CodiPuntoVenta"],
+                        CodiRuta = (int)x["CodiRuta"],
+                        CodiServicio = (int)x["CodiServicio"],
+                        CodiSucursal = (int)x["CodiSucursal"],
+                        Dias = (int)x["Dias"],
+                        FechaProgramacion = (string)x["FechaProgramacion"],
+                        HoraPartida = (string)x["HoraPartida"],
+                        HoraProgramacion = (string)x["HoraProgramacion"],
+                        NomDestino = (string)x["NomDestino"],
+                        NomOrigen = (string)x["NomOrigen"],
+                        NomPuntoVenta = (string)x["NomPuntoVenta"],
+                        NomRuta = (string)x["NomRuta"],
+                        NomServicio = (string)x["NomServicio"],
+                        NomSucursal = (string)x["NomSucursal"],
+                        NroRuta = (int)x["NroRuta"],
+                        NroRutaInt = (long)x["NroRutaInt"],
+                        NroViaje = (long)x["NroViaje"],
+                        PlacaBus = (string)x["PlacaBus"],
+                        PlanoBus = (string)x["PlanoBus"],
+                        RazonSocial = (string)x["RazonSocial"],
+                        StOpcional = (string)x["StOpcional"]
+                    }).ToList();
+                    return Json(items, JsonRequestBehavior.AllowGet);
                 }
                 else
                 {
--- SisComWeb.Aplication/Models/Itinerario.cs
+++ SisComWeb.Aplication/Models/Itinerario.cs
@@ -7,5 +7,33 @@ namespace SisComWeb.Aplication.Models
 {
     public class Itinerario
     {
+        public int AsientosVendidos { get; set; }
+        public string CapacidadBus { get; set; }
+        public string CodiBus { get; set; }
+        public int CodiDestino { get; set; }
+        public int CodiEmpresa { get; set; }
+        public int CodiOrigen { get; set; }
+        public int CodiProgramacion { get; set; }
+        public int CodiPuntoVenta { get; set; }
+        public int CodiRuta { get; set; }
+        public int CodiServicio { get; set; }
+        public int CodiSucursal { get; set; }
+        public int Dias { get; set; }
+        public string FechaProgramacion { get; set; }
+        public string HoraPartida { get; set; }
+        public string HoraProgramacion { get; set; }
+        public string NomDestino { get; set; }
+        public string NomOrigen { get; set; }
+        public string NomPuntoVenta { get; set; }
+        public string NomRuta { get; set; }
+        public string NomServicio { get; set; }
+        public string NomSucursal { get; set; }
+        public int NroRuta { get; set; }
+        public long NroRutaInt { get; set; }
+        public long NroViaje { get; set; }
+        public string PlacaBus { get; set; }
+        public string PlanoBus { get; set; }
+        public string RazonSocial { get; set; }
+        public string StOpcional { get; set; }
     }
 }
