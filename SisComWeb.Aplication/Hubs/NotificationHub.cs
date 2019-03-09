0fdb57945176c1b3b2f631d5d84cb05f0b7e2675
Hector Salazar <hectorsalazarv@outlook.com>
2019-03-09 14:09:14 -0500

    implement signal r to bloquear and liberar

---
+++ SisComWeb.Aplication/Hubs/NotificationHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SisComWeb.Aplication.Hubs
+{
+    public class NotificationHub : Hub
+    {
+
+        public void Bloquear(string nroViaje, string fechaViaje, string nroAsiento)
+        {
+            Clients.All.bloquearBus(nroViaje, fechaViaje, nroAsiento);
+        }
+
+        public void Liberar(string nroViaje, string fechaViaje, string nroAsiento)
+        {
+            Clients.All.liberarBus(nroViaje, fechaViaje, nroAsiento);
+        }
+    }
+}
